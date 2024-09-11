741adaa7e5bd2b563d11c0d58850b7875299d661
Nourhoballah <nourhoballah@outlook.com>
2024-09-11 16:18:39 -0400

    Update ex1.py

--- ex1.py
+++ ex1.py
@@ -1,5 +1,13 @@
 # TODO: Créer un script permettant le formattage du livre des records des JO.
 
+''''  Demander la nationalité de l'athlète
+- Demander le nom de l'athlète
+- Demander la date du record
+- Demander la discipline
+- Demander la catégorie, qui peut être nulle
+- Demander le record
+'''
+
 
 country = input("De quelle nationalité est l'athlète ? ")
 athlete = input("Quelle est le nom de l'athelète ?")
