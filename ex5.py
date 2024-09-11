6bf180b1a637ca7545c3fe8bbdffe21c8cfa1924
Nourhoballah <nourhoballah@outlook.com>
2024-09-11 17:47:14 -0400

    Update ex5.py

--- ex5.py
+++ ex5.py
@@ -1,5 +1,38 @@
 #TODO: Analyser la chaîne de caractères saisie et compter le nombre de médailles.
 #      Attention si la chaîne est invalide, un message d'erreur est attendu.
+# Exercice 5: 
+"""
+Afin de faciliter le suivi et le calcul des médailles gagnées par pays, le comité vous demande de créer un script permettant de traiter la chaîne de caractères suivante: `GBBSBG`.  
+- `G` signifie une médaille d'or
+- `S` signifie une médaille d'argent
+- `B` signifie une médaille de bronze
 
-country = ...
-code_medals = ...
+Consignes: 
+- Demander la chaîne de caractères représentant le résultat d'un pays
+- Afficher le nombre de médailles de ce pays
+- Si un caractère inattendu se glisse dans la chaîne, veuillez afficher une erreur
+
+Exemple: 
+
+![Exemple gif ex5](./assets/ex5.gif)
+
+| Sorties | Entrées |
+|:-|:-|
+| Résultat du pays: | GBBSBG |
+| Médailles:<br>- Or: 2<br>- Argent: 1<br>- Bronze: 2 |
+
+
+| Sorties | Entrées |
+|:-|:-|
+| Résultat du pays: | Ceci est une chaîne invalide |
+| Veuillez entrer une chaîne valide. |
+
+AGSB        GSB
+"""
+Pays=input("Quel est votre pays?")
+codeMedaille=input("Quel est le résultat de votre pays? ")
+
+for x in codeMedaille:
+     if x !="G" and "S" and "B":
+        print ("erreur de code")
+
