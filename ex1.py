6d46eeedff19d1a253915b2e625f0e129479259c
Mehdi-Chahir <babassako@hotmail.com>
2024-09-11 16:17:30 -0400

    Update ex1.py

--- ex1.py
+++ ex1.py
@@ -4,4 +4,4 @@
 country = input("De quelle nationalité est l'athlète ? ")
 athlete = input("Quelle est le nom de l'athelète ?")
 date = input("Quelle est la date du record ?")
-aa= input("Mehdi")
+discipline= input("Quelle est la discipline ?")
